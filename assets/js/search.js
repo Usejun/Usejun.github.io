d4ec317edcf1c520d11ec37b0e8c746bdc5d4bd5
Usejun <84970153+Usejun@users.noreply.github.com>
2025-11-30 13:45:45 +0000

    Initial commit6

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,16 +1,24 @@
-// lunr 한국어 적용
+// 한국어 tokenizer 적용
 lunr.ko(lunr);
 
-fetch("/assets/js/search-data.json")
-  .then(r => r.json())
-  .then(pages => {
+(function() {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/search-data.json');
+  request.responseType = 'json';
+
+  request.onload = () => {
+    const pages = request.response;
+
     const index = lunr(function () {
       this.use(lunr.ko);
-      this.ref("url");
-      this.field("title", { boost: 10 });
-      this.field("content");
+      this.ref('id');
+      this.field('title', { boost: 10 });
+      this.field('content');
 
-      pages.forEach(page => this.add(page));
+      pages.forEach((page, idx) => {
+        page.id = idx;
+        this.add(page);
+      });
     });
 
     const input = document.querySelector('#search-input');
@@ -18,11 +26,26 @@ fetch("/assets/js/search-data.json")
 
     input.addEventListener('input', function () {
       const query = input.value.trim();
+
+      if (!query) {
+        results.innerHTML = '';
+        return;
+      }
+
       const hits = index.search(query);
 
       results.innerHTML = hits.map(hit => {
-        const page = pages.find(p => p.url === hit.ref);
-        return `<li><a href="${page.url}">${page.title}</a></li>`;
+        const page = pages[hit.ref];
+        return `
+          <li class="search-result">
+            <a href="${page.url}">
+              <span class="search-result-title">${page.title}</span>
+              <span class="search-result-excerpt">${page.excerpt}</span>
+            </a>
+          </li>`;
       }).join('');
     });
-  });
+  };
+
+  request.send();
+})();
